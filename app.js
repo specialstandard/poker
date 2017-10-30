1e4e15a5e1408daa8136c66b53b003a90a6e643b
specialstandard <italissimo@gmail.com>
2017-10-30 22:10:21 +1100

    determines current round

--- app.js
+++ app.js
@@ -1,57 +1,79 @@
-const tesseract = require('node-tesseract');
+const robot = require("robotjs");
 const screenshot = require('screenshot-node');
+const tesseract = require('node-tesseract');
 const Observable = require('rxjs/Observable').Observable;
-
-Round = {
-    PRE: 0,
-    FLOP: 1,
-    TURN: 2,
-    RIVER: 3
+const Rx = require('rxjs')
+Rounds = {
+    Pre: 0,
+    Flop: 1,
+    Turn: 2,
+    River: 3
 }
 
 const myCardWidth = 84
 const tableCardWidth = 91
 
-let currentRound = Round.PRE
-let isMyTurn = false;
+let acted = false
+let currentRound
+// let isMyTurn = false;
 let myCards = []
 let pot
 let stack
 let tableCards = []
 
-console.log('currentRound: ', currentRound)
+let myTurnPos = {
+    x: 1180,
+    y: 862
+}
+console.log('init')
 
-for(let i = 0; i < 2; i++) {
-    getMyCard(i).subscribe((res) => {
-        myCards[i] = res
-        console.log('myCards: ', myCards)
-    })
+setInterval(() => {
+    if(isMyTurn()) {
+        console.log('\nisMyTurn now')
+        getMyCards()
+        getTableCards()
+    } else {
+        // console.log('not isMyTurn now')
+    }
+}, 1000)
+
+function isMyTurn() {
+    return robot.getPixelColor(myTurnPos.x, myTurnPos.y) === 'ffffff'
 }
 
-getPot().subscribe((res) => {
-    pot = res
-    console.log('pot: ', pot)
-})
-getStack().subscribe((res) => {
-    stack = res
-    console.log('stack: ', stack)
-})
-for(let i = 0; i < 5; i++) {
-    getTableCard(i).subscribe((res) => {
-        tableCards[i] = res
-        console.log('tableCards: ', tableCards)
-    })
-}
+// console.log('currentRound: ', currentRound)
 
-function getMyCard(cardPos) {
+// for(let i = 0; i < 2; i++) {
+//     getMyCard(i).subscribe((res) => {
+//         myCards[i] = res
+//         console.log('myCards: ', myCards)
+//     })
+// }
+
+// getPot().subscribe((res) => {
+//     pot = res
+//     console.log('pot: ', pot)
+// })
+// getStack().subscribe((res) => {
+//     stack = res
+//     console.log('stack: ', stack)
+// })
+// for(let i = 0; i < 5; i++) {
+//     getTableCard(i).subscribe((res) => {
+//         tableCards[i] = res
+//         console.log('tableCards: ', tableCards)
+//     })
+// }
+
+function getMyCard(cardNum) {
     return Observable.create((observer) => {
-        let x = 878 + cardPos * myCardWidth
+        let x = 878 + cardNum * myCardWidth
         let y = 640
         let width = 29
         let height = 32
 
         let path = __dirname + '\\img\\'
-        let filename = 'myCard' + cardPos + '.jpg'
+        let filename = 'myCard' + cardNum + '.jpg'
         let destination = path + filename
         const options = {
             psm: 10, // allow single character recognition
@@ -76,6 +98,46 @@ function getMyCard(cardPos) {
         });
     })
 
+}
+
+function getMyCards() {
+    Rx.Observable.forkJoin(
+        getMyCard(0),
+        getMyCard(1)
+    ).subscribe((res) => {
+        myCards = res
+        console.log(myCards)
+    })
+}
+
+function getTableCards() {
+    Rx.Observable.forkJoin(
+        getTableCard(0),
+        getTableCard(1),
+        getTableCard(2),
+        getTableCard(3),
+        getTableCard(4)
+    ).subscribe((res) => {
+        if (res[1] == ';') {
+            // Preflop
+            currentRound = Rounds.Pre
+            tableCards = [null, null, null, null, null]
+        } else if (res[3] === 'u') {
+            // Flop
+            currentRound = Rounds.Flop
+            tableCards = [res[0], res[1], res[2], null, null]
+        } else if (res[4] === '_') {
+            // Turn
+            currentRound = Rounds.Turn
+            tableCards = [res[0], res[1], res[2], res[3], null]
+        } else {
+            // River
+            currentRound = Rounds.River
+            tableCards = [res[0], res[1], res[2], res[3], res[4]]
+        }
+        console.log('currentRound: ', currentRound)
+        console.log('tableCards: ', tableCards)
+    })
 }
 function getPot() {
     return Observable.create((observer) => {
@@ -147,15 +209,15 @@ function getStack() {
 
 }
 
-function getTableCard(cardPos) {
+function getTableCard(cardNum) {
     return Observable.create((observer) => {
-        let x = 737 + cardPos * tableCardWidth
+        let x = 737 + cardNum * tableCardWidth
         let y = 375
         let width = 29
         let height = 32
 
         let path = __dirname + '\\img\\'
-        let filename = 'tableCard' + cardPos + '.jpg'
+        let filename = 'tableCard' + cardNum + '.jpg'
         let destination = path + filename
         const options = {
             psm: 10, // allow single character recognition
